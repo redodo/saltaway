751e6501c96b279df612b0288bd7c1fc35aa3199
redodo <me@redodo.io>
2018-04-13 16:17:23 +0200

    Change --find option to -q and add different repository flags

--- saltaway/cli.py
+++ saltaway/cli.py
@@ -5,14 +5,26 @@ import saltaway
 
 @click.command()
 @click.argument('url')
-@click.option('--find', '-f', is_flag=True, default=False,
+@click.option('-q', is_flag=True, default=False,
               help="Find the most recently archived version of the URL")
-def cli(url, find):
+@click.option('-I', is_flag=True, default=False, help="Use archive.is")
+@click.option('-A', is_flag=True, default=False, help="Use web.archive.org")
+def cli(url, q, i, a):
+    """Archive web pages at various archives."""
 
-    if find:
-        for repo, datetime, archive_url in saltaway.pull(url):
-            click.echo("{} | {}".format(datetime.isoformat(), archive_url))
+    repos = []
+    if a:
+        repos.append(saltaway.InternetArchive)
+    if i:
+        repos.append(saltaway.ArchiveIs)
+
+    if len(repos) == 0:
+        repos = saltaway.REPOSITORIES
+
+    if q:
+        for repo, datetime, archive_url in saltaway.pull(url, repos=repos):
+            click.echo("{} {}".format(datetime.isoformat(), archive_url))
 
     else:
-        for repo, archive_url in saltaway.push(url):
+        for repo, archive_url in saltaway.push(url, repos=repos):
             click.echo(archive_url)
