ad3733e8e66c580e99860bc56f1dd7d8c2821896
redodo <me@redodo.io>
2018-04-14 13:39:06 +0200

    Improve duration parsing to accept non-spaced durations

--- saltaway/utils.py
+++ saltaway/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import itertools
 from datetime import timedelta
 
 import pendulum
@@ -14,23 +15,31 @@ _TIME_DENOMINATIONS = {
 
 
 def duration_from_string(value):
-    parts = [(float(e[:-1]), e[-1]) for e in value.split()]
+    value = value.replace(' ', '')
+    parts = [''.join(e) for _, e in itertools.groupby(value, key=str.isdigit)]
+
+    if len(parts) % 2 != 0:
+        raise ValueError("Incorrect duration format")
+
+    # map lengths to denominations
+    parts = zip(parts[::2], parts[1::2])
 
     duration_input = {}
     for length, char in parts:
         try:
             denomination = _TIME_DENOMINATIONS[char]
-            duration_input[denomination] = length
+            duration_input[denomination] = float(length)
         except KeyError:
-            raise ValueError("unknown time denomination '%s'" % char)
+            raise ValueError("Unknown time denomination '%s'" % char)
 
     return pendulum.Interval(**duration_input)
 
 
 def parse_duration(value):
-    if isinstance(value, int):
+    if isinstance(value, (int, float)):
         return pendulum.interval(seconds=value)
-    if isinstance(value, (timedelta, pendulum.Interval,)):
+    if isinstance(value, (timedelta, pendulum.Interval)):
         return pendulum.interval.instance(value)
     if isinstance(value, str):
         return duration_from_string(value)
+    raise ValueError("Could not parse value '%s' as a duration" % value)
