1bfcd14d668229ad0840ac860fbc802a5af2f8af
redodo <me@redodo.io>
2018-04-14 14:04:32 +0200

    Add max_workers param for Python 3.4

--- saltaway/api.py
+++ saltaway/api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from .repositories import ArchiveIs, InternetArchive
 
@@ -10,24 +10,27 @@ REPOSITORIES = (
 )
 
 
+MAX_WORKERS = min(len(REPOSITORIES), 4)
+
+
 def push(url, repos=REPOSITORIES, max_age=0):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(MAX_WORKERS) as executor:
         future_to_repo = {
             executor.submit(repo().push, url, max_age=max_age): repo
             for repo in repos
         }
-        for future in concurrent.futures.as_completed(future_to_repo):
+        for future in as_completed(future_to_repo):
             yield future_to_repo[future], future.result()
 
 
 def pull(url, repos=REPOSITORIES):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(MAX_WORKERS) as executor:
         future_to_repo = {
             executor.submit(repo().pull, url): repo
             for repo in repos
         }
-        for future in concurrent.futures.as_completed(future_to_repo):
+        for future in as_completed(future_to_repo):
             try:
-                yield (future_to_repo[future], *future.result())
+                yield [future_to_repo[future]].extend(future.result())
             except NotImplementedError:
                 pass
