0fe53beeb430606768e277a8879e87bce432efb7
redodo <me@redodo.io>
2018-04-14 14:05:20 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,36 @@ setup(
     license='MIT',
     packages=['saltaway'],
     install_requires=[
-        'lxml',
-        'click',
-        'requests',
-        'pendulum',
+        'lxml>=4.0',
+        'click>=5.1',
+        'requests>=2.0',
+        'pendulum>=1.3',
     ],
-    entry_points='''
-        [console_scripts]
-        saltaway=saltaway.cli:cli
-    '''
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'coverage',
+            'tox',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'saltaway = saltaway.cli:cli',
+        ],
+    },
 )
